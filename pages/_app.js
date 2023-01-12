e602725cfc8305ded8c23e4c1d83e88fb51e3375
Sumer Patel <patelsum@oregonstate.edu>
2023-01-12 14:46:11 +0530

    Changed render view to accomodate Material UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createCache({ key: 'css'});
+
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,56 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render () {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// New version of the render page
+MyDocument.getInitialProps = async (ctx) => {
+  
+  const originalRenderPage = ctx.renderPage;
+  const cache = createCache({ key: 'css' });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+  
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhance: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  }
+}
+
+// Changed the way that we render the document
