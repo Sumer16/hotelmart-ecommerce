552876c745146531c2d6107a48bccaf4adc7bf1f
Sumer Patel <patelsum@oregonstate.edu>
2023-01-12 20:58:15 +0530

    Integrated Sanity with NextJS app

---
+++ sanity/schemas/product.js
@@ -0,0 +1,54 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'brand',
+            title: 'Brand',
+            type: 'string',
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+        },
+        {
+            name: 'countInStock',
+            title: 'CountInStock',
+            type: 'number'
+        },
+    ]
+}
